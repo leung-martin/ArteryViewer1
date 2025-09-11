1e01fbba0c97daa520b192fcb7d92436da53c491
leung-martin <leung.martin@outlook.com>
2025-09-10 22:17:00 -0400

    fix camera orientation

--- docs/app.js
+++ docs/app.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x2a2a2a);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 4, 7);
+camera.position.set(0, 6.5, 7);
 // Remove initial rotation setting - will set it after OrbitControls
 
 
